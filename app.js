d0dd7807685a3c4e6e641380c364449ce524d793
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-07-06 16:23:05 -0300

    Server creation

---
+++ app.js
@@ -0,0 +1,34 @@
+'use stric'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const configure = require('./src/configure/config');
+
+const app = express();
+const router = express.Router();
+
+//connecta no banco
+mongoose.connect(configure.connectionString,{useNewUrlParser: true, useUnifiedTopology: true});
+
+
+//carrega as rotas
+const productRoute = require('./src/app/route/product-route')
+
+app.use(bodyParser.json({
+    limit: '5mb'
+}));// Todo conteudo passa para JSON
+app.use(bodyParser.urlencoded({
+     extended: false 
+}));
+// Habilita o CORS
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, x-Request-With, Content-type,Accept,x-access-token');
+    res.header('Access-control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+//app.use('/', indexRoute);
+app.use('/product', productRoute);
+module.exports = app;
--- bin/server.js
+++ bin/server.js
@@ -1,21 +1,54 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-var express = require ('express');
-var bodyParser = require('body-parser');
-const app = express();
-const config = require('../src/configure/config')
-const mongoose = require('mongoose')
-//PERSISTÊNCIA
-mongoose.connect(config.connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+//Força o JS a ser criterioso
 
-//Configuração do server para usar body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const app = require('../app');
+const http = require('http');//Vai buscar na pasta node_modules
+const debug = require('debug');//Vai buscar na pasta node_modules
+//Exemplo - importar arquivo local
+//const xpto = requeire('./xpto'); Vai buscar na pasta da aplicação
 
-//Definindo a porta via arquivo de configuração
-var port = process.env.port || 3000;
+const port = normalizePort(process.env.port || '3000');
 
-app.listen(port, () => {
-    console.log('Server up and running!');
+const server = http.createServer(app);//Criando servidor -  modelo mvc
 
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+console.log('Api rodando na porta ' + port);
+
+function normalizePort(val){
+    const port = parseInt(val, 10);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >= 0){
+        return port;
+    }
+    return false;
+};
+function onError(error){
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+    const bind =
+    typeof port == 'string' ? 'Pipe' + port: 'Port' + port;
+    
+    switch (error.code){
+        case 'EACCES' : //Erro de permissão
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINNUSE' : 
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default :
+            throw error;
+    }
+}
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string' 
+        ? 'pipe' + addr
+        : 'port' + addr.port;
+        debug('Listening on ' + bind);
+}
